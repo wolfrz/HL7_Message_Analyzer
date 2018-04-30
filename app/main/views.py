895cb2406c546b88cde3ced11f610f77194291e8
wolfrz <wolfrz@163.com>
2018-04-30 12:29:11 +0800

    HTML页面模板构建
    Signed-off-by: wolfrz <wolfrz@163.com>

--- app/main/views.py
+++ app/main/views.py
@@ -6,6 +6,25 @@ from flask import render_template
 from . import main
 
 @main.route('/')
-@main.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html'), 200
+
+@main.route('/login')
+def login():
+    return render_template('login.html'), 200
+
+@main.route('/standard')
+def standard():
+    return render_template('message.html'), 200
+
+@main.route('/message')
+def message():
+    return render_template('message.html'), 200
+
+@main.route('/help')
+def help():
+    return render_template('help.html'), 200
+
+@main.errorhandler(404)
+def page_not_found():
+    return render_template('404.html'), 404
--- config.py
+++ config.py
@@ -6,6 +6,16 @@ import os
 project_dir = os.path.abspath(os.path.dirname(__file__))
 
 class configure(object):
+    # mysql的hl7_std数据库配置参数
+    DB_MYSQL_HL7_STD_HOST = '127.0.0.1'
+    DB_MYSQL_HL7_STD_PORT = 3306
+    DB_MYSQL_HL7_STD_USERNAME = 'root'
+    DB_MYSQL_HL7_STD_PASSWORD = 'root'
+    DB_MYSQL_HL7_STD_DATABASE = 'hl7_std'
+    # mongo的hl7_msg数据库配置参数
+    DB_MONGO_HL7_MSG_HOST = '127.0.0.1'
+    DB_MONGO_HL7_MSG_PORT = 27017
+    DB_MONGO_HL7_MSG_DATABASE = 'hl7_msg'
 
     @staticmethod
     def initialization(application):
