e31cd6455e8b413e3099770284e979ee880be559
wolfrz <wolfrz@163.com>
2018-05-01 10:07:10 +0800

    简易水平导航栏设计
    Signed-off-by: wolfrz <wolfrz@163.com>

--- app/main/views.py
+++ app/main/views.py
@@ -7,23 +7,23 @@ from . import main
 
 @main.route('/')
 def index():
-    return render_template('index.html'), 200
+    return render_template('index.html', page='home'), 200
 
 @main.route('/login')
 def login():
-    return render_template('login.html'), 200
+    return render_template('login.html', page='login'), 200
 
 @main.route('/standard')
 def standard():
-    return render_template('message.html'), 200
+    return render_template('standard.html', page='standard'), 200
 
 @main.route('/message')
 def message():
-    return render_template('message.html'), 200
+    return render_template('message.html', page='message'), 200
 
 @main.route('/help')
 def help():
-    return render_template('help.html'), 200
+    return render_template('help.html', page='help'), 200
 
 @main.errorhandler(404)
 def page_not_found():
